13160725462d7910c8301c662dda8fe42fe49911
Juh Linux <julia.ingridsantos.7@gmail.com>
2022-03-03 09:37:16 -0300

    fix: add IsOptional no updateRequest

--- src/controllers/dto/UserDto.ts
+++ src/controllers/dto/UserDto.ts
@@ -1,5 +1,5 @@
 import { User } from '../../entities/User';
-import {IsEmail,IsNotEmpty, Min, Max, Length} from "class-validator"
+import {IsEmail,IsNotEmpty, Min, Max, Length, IsOptional} from "class-validator"
 
 export class UserRequest {
     @IsNotEmpty()
@@ -19,23 +19,25 @@ export class UserRequest {
 }
 
 export class UserUpdateRequest {
-    @IsNotEmpty()
     id:string
     
+    @IsOptional()
     @IsNotEmpty()
     @Length(5,50)
     name: string
     
+    @IsOptional()
     @IsNotEmpty()
     @Length(5,60)
     @IsEmail()
     email:string
 
+    @IsOptional()
     @IsNotEmpty()
     user_type_id:string
 
+    @IsOptional()
     @IsNotEmpty()
-    @Min(6)
-    @Max(16)
+    @Length(6,16)
     password: string
 }
