80b8dcf36900f8a11961ffdbc1f181ec3ecc5ea0
Juh Linux <julia.ingridsantos.7@gmail.com>
2022-03-07 11:55:11 -0300

    feat: adcionando o cors

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,17 @@ import "reflect-metadata"
 import express from "express"
 import "./database"
 import { routes } from "./routes"
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
+
 app.use(express.json())
 
+
 app.use(routes)
+
 
 
 app.listen(9000, ()=>{
